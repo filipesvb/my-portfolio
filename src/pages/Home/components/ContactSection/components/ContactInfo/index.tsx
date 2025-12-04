f08f8b236a28905c50f7c18f88c74238aa6ee922
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-12-04 16:41:07 +0000

    fix: corrected color of email and phone info

--- src/pages/Home/components/ContactSection/components/ContactInfo/index.tsx
+++ src/pages/Home/components/ContactSection/components/ContactInfo/index.tsx
@@ -1,34 +1,33 @@
-import { Phone } from "@/components/icons/Phone";
 import { ExternalLink, LucideMail, PhoneCallIcon } from "lucide-react";
 
 const ContactInfo = () => {
   return (
-    <div className="border-foreground mt-4 space-y-5 rounded-lg border-2 p-8 md:max-w-[390px]">
-      <div className="flex items-center gap-2">
+    <div className="border-foreground text-foreground mt-4 space-y-5 rounded-lg border-2 p-8 md:max-w-[390px]">
+      <div className="flex items-center gap-2 text-inherit">
         <div className="flex-1/5 flex items-center">
           <LucideMail className="stroke-foreground" />
         </div>
-        <div className="flex-4/5 flex items-center gap-2">
+        <div className="flex-4/5 flex items-center gap-2 text-inherit">
           <a
-            className="text-foreground flex gap-2"
+            className="flex gap-2 text-inherit"
             href="mailto:fvb.boas@gmail.com"
           >
-            <span>fvb.boas@gmail.com </span>
-            <ExternalLink />
+            <span className="text-inherit">fvb.boas@gmail.com </span>
+            <ExternalLink className="stroke-foreground" />
           </a>
         </div>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 text-inherit">
         <div className="flex-1/5 flex items-center">
           <PhoneCallIcon className="stroke-foreground" />
         </div>
-        <div className="flex-4/5 flex">
+        <div className="flex-4/5 flex text-inherit">
           <a
-            className="text-foreground flex items-center gap-2"
+            className="flex items-center gap-2 text-inherit"
             href="tel:+551999734-0189"
           >
-            <span className="truncate">+55 (19)99734-0189</span>
-            <ExternalLink />
+            <span className="truncate text-inherit">+55 (19)99734-0189</span>
+            <ExternalLink className="stroke-foreground" />
           </a>
         </div>
       </div>
