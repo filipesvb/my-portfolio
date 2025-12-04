ef9fe18a4b644272d7c8a07de29a1e092afb57e4
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-12-04 19:59:44 -0300

    fix: ensured whatsapp link works both in mobile and desktop

--- src/pages/Home/components/ContactSection/components/ContactInfo/index.tsx
+++ src/pages/Home/components/ContactSection/components/ContactInfo/index.tsx
@@ -4,10 +4,10 @@ const ContactInfo = () => {
   return (
     <div className="border-foreground text-foreground mt-4 space-y-5 rounded-lg border-2 p-8 md:max-w-[390px]">
       <div className="flex items-center gap-2 text-inherit">
-        <div className="flex-1/5 flex items-center">
+        <div className="flex flex-1/5 items-center">
           <LucideMail className="stroke-foreground" />
         </div>
-        <div className="flex-4/5 flex items-center gap-2 text-inherit">
+        <div className="flex flex-4/5 items-center gap-2 text-inherit">
           <a
             className="flex gap-2 text-inherit"
             href="mailto:fvb.boas@gmail.com"
@@ -18,13 +18,14 @@ const ContactInfo = () => {
         </div>
       </div>
       <div className="flex items-center gap-2 text-inherit">
-        <div className="flex-1/5 flex items-center">
+        <div className="flex flex-1/5 items-center">
           <PhoneCallIcon className="stroke-foreground" />
         </div>
-        <div className="flex-4/5 flex text-inherit">
+        <div className="flex flex-4/5 text-inherit">
           <a
             className="flex items-center gap-2 text-inherit"
-            href="https://api.whatsapp.com/send?phone=+551999734-0189"
+            target="_blank"
+            href="https://wa.me/5519997340189"
           >
             <span className="truncate text-inherit">+55 (19)99734-0189</span>
             <ExternalLink className="stroke-foreground" />
