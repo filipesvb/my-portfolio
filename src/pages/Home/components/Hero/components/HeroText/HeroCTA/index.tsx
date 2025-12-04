f17e207cab4e4b919cf2332ba7d530e7c15559fd
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-12-04 13:53:51 +0000

    feat: added Link to /contact on Hero CTA

--- src/pages/Home/components/Hero/components/HeroText/HeroCTA/index.tsx
+++ src/pages/Home/components/Hero/components/HeroText/HeroCTA/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import { LiquidButton } from "@/components/ui/shadcn-io/liquid-button";
+import { Link } from "react-router";
 
 const HeroCTA = ({
   orcamentoTxt,
@@ -9,11 +10,19 @@ const HeroCTA = ({
   ctaTxt: string;
 }) => {
   return (
-    <div className="flex flex-col space-y-3 items-center md:items-start">
-      <h3 className="font-bold text-2xl leading-none text-foreground">{orcamentoTxt}</h3>
-      <LiquidButton variant={"custom"} size={"lg"} className="text-foreground border-muted-foreground">
-        {ctaTxt}
-      </LiquidButton>
+    <div className="flex flex-col items-center space-y-3 md:items-start">
+      <h3 className="text-foreground text-2xl font-bold leading-none">
+        {orcamentoTxt}
+      </h3>
+      <Link to={"/contato"}>
+        <LiquidButton
+          variant={"custom"}
+          size={"lg"}
+          className="text-foreground border-muted-foreground"
+        >
+          {ctaTxt}
+        </LiquidButton>
+      </Link>
     </div>
   );
 };
