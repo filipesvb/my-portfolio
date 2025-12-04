98e150a37d1a8cbbb31a4df263181e07c166c41a
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-12-04 13:41:38 +0000

    fix: ensured service items are responsive and fully visible in desktop viewport

--- src/pages/Home/components/ServiceSection/components/ServiceSingle/index.tsx
+++ src/pages/Home/components/ServiceSection/components/ServiceSingle/index.tsx
@@ -9,11 +9,35 @@ interface ServiceSingleProps {
 
 const ServiceSingle = ({ title, description, image }: ServiceSingleProps) => {
   return (
-    <div className="flex-1">
+    <>
+      <div className="flex-1 md:hidden">
+        <motion.div
+          className="relative"
+          whileInView={{ right: "0%", opacity: 1 }}
+          initial={{ right: "-100%", opacity: 0 }}
+          transition={{
+            right: { duration: 0.65, ease: "easeOut" },
+            opacity: { duration: 2.85, ease: "easeOut" },
+          }}
+          viewport={{ once: true }}
+        >
+          <div className="h-100 w-full">
+            <img
+              src={image}
+              alt={title}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+          <CardTitle className="text-foreground">{title}</CardTitle>
+          <p className="text-muted-foreground">{description}</p>
+        </motion.div>
+      </div>
+
+      {/* DESKTOP */}
       <motion.div
-        className="relative"
-        whileInView={{ right: "0%", opacity: 1 }}
-        initial={{ right: "-100%", opacity: 0 }}
+        className="relative hidden md:block"
+        whileInView={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
         transition={{
           right: { duration: 0.65, ease: "easeOut" },
           opacity: { duration: 2.85, ease: "easeOut" },
@@ -30,7 +54,7 @@ const ServiceSingle = ({ title, description, image }: ServiceSingleProps) => {
         <CardTitle className="text-foreground">{title}</CardTitle>
         <p className="text-muted-foreground">{description}</p>
       </motion.div>
-    </div>
+    </>
   );
 };
 
