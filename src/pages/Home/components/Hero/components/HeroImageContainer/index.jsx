0f13f3eb0d5d3b93b165a99811da3e7359b1cbd5
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-12-08 14:05:57 +0000

    feat: added link to hero imagem in desktop

--- src/pages/Home/components/Hero/components/HeroImageContainer/index.jsx
+++ src/pages/Home/components/Hero/components/HeroImageContainer/index.jsx
@@ -1,19 +1,22 @@
 import HoverableWithBall from "@/components/HoverableWithBall";
 import Me from "@/assets/imgs/me.jpg";
+import { Link } from "react-router";
 
 const HeroImageContainer = () => {
   return (
-    <div className="flex-3/5 flex justify-center items-center relative hidden md:flex ">
-      <HoverableWithBall
-        ballText="hero_image_cta"
-        className="rounded-full w-2/3 pt-[65%]  relative"
-      >
-        <div className="absolute top-0 left-0 h-full w-full pointer-events-none">
-          <div className="relative overflow-hidden h-full w-full rounded-full">
-            <img src={Me} className="absolute top-0 left-0" />
+    <div className="flex-3/5 relative flex hidden items-center justify-center md:flex">
+      <Link to={"/contato"} className="h-full w-2/3 rounded-full">
+        <HoverableWithBall
+          ballText="hero_image_cta"
+          className="relative h-full w-full rounded-full"
+        >
+          <div className="pointer-events-none absolute left-0 top-0 h-full w-full">
+            <div className="relative h-full w-full overflow-hidden rounded-full">
+              <img src={Me} className="absolute left-0 top-0" />
+            </div>
           </div>
-        </div>
-      </HoverableWithBall>
+        </HoverableWithBall>
+      </Link>
     </div>
   );
 };
