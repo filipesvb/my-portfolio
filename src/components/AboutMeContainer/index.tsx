c2ab9bc1553ca5122cefb7317e5b26a195e75313
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-12-08 16:56:03 +0000

    feat: added image to AboutMeContainer

--- src/components/AboutMeContainer/index.tsx
+++ src/components/AboutMeContainer/index.tsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router";
 import AboutMe from "../AboutMe";
-import { Button } from "../ui/button";
+
+import Me2 from "@/assets/imgs/eu2.jpg";
 
 const AboutMeContainer = ({ compact }: { compact: boolean }) => {
   return (
     <div className="border-muted-foreground flex flex-col border-2 md:flex-row">
-      <div className="h-[500px] p-4 md:flex-1">
-        <div className="border-muted-foreground h-full border-2 border-dotted"></div>
+      <div className="h-full p-4 md:flex-1">
+        <div className="border-muted-foreground h-full">
+          <img
+            src={Me2}
+            alt="Filipe Vilas Bôas"
+            className="h-full w-full object-cover"
+          />
+        </div>
       </div>
       <div className="flex flex-1 flex-col justify-between gap-8 px-6 py-4">
         <AboutMe compact={compact} />
-        
       </div>
     </div>
   );
